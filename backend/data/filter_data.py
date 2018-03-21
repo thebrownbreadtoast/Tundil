d14241f42cefaf8d167e15432930b27b73af6cd2
dadwalakshay <akshay.dadwal.rajput@gmail.com>
2018-03-21 21:19:28 +0530

    backend branch init

---
+++ backend/data/filter_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+class filter_data(object):
+
+	def data_filter(self,*args):
+		dataset = pd.read_csv('raw_dataset.csv')
+		source = args[0]
+		filtered_dataset = dataset['source'] == source
+		filtered_text = dataset[filtered_dataset]['text']
+		return filtered_text
+
+	def file_write(self,*args):
+		filename = args[0]
+		data = self.data_filter(filename)
+		with open(filename+'.txt', 'w') as file:
+			for line in data:
+				file.write(line)
+			file.close()
+
+if __name__ == '__main__':
+	file = filter_data()
+	file.file_write('human')
+	file.file_write('robot')
+	
+	
