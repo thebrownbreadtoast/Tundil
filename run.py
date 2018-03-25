c785ce662b244b4fce5549d92d9e496d273ed8d9
dadwalakshay <akshayd00@outlook.com>
2018-03-25 17:27:26 +0000

    commit from pythonanywhere

--- run.py
+++ run.py
@@ -1,5 +1,2 @@
 from backend.chatbot_api import chat_init
-
-if __name__ == '__main__':
-    app = chat_init().server(__name__)
-    app.run(host='0.0.0.0',debug=True,port=7464)
+app = chat_init().server(__name__)
