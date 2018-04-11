1ba5a1de78bf72543f24e3f0f87f3a2a47c0c5e3
aditya-verma <aditya-verma@users.noreply.github.com>
2018-04-12 00:02:44 +0530

    Update
    -removed port number from ip

---
+++ frontend/app/src/main/java/com/example/aditya/Tundil/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.example.aditya.Tundil;
+
+/**
+ * Created by ADITYA on 22 Mar 2018.
+ */
+
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
+        this.message = message;
+    }
+}
